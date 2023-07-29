3dd8ce513a9aabba902cbbc4ed4130752c33563e
Luke Gaunt <30408050+FinchyG@users.noreply.github.com>
2023-07-29 19:35:12 +0100

    first commit

---
+++ fixtureAlgorithm.js
@@ -0,0 +1,13 @@
+var teams = [1,2,3,4,5];
+var fixtures = [];
+
+function fixtureAlgorithm() {
+  
+  if(teams.length % 2 == 1) {
+    console.log("odd number of teams");
+  }
+}
+
+fixtureAlgorithm();
+
+
