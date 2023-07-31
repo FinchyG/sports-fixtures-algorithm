09c67b44f736bcbf243bfe89dc0ba05a17dee4be
Luke Gaunt <30408050+FinchyG@users.noreply.github.com>
2023-07-31 07:51:42 +0100

    working algorithm with HTML file added for ease of testing

--- fixtureAlgorithm.js
+++ fixtureAlgorithm.js
@@ -1,21 +1,35 @@
-var teams = [1,2,3,4,5];
+// Variables storing teams fixtures
+var teams = [1, 2, 3, 4, 5];
+var middleOfTeamsArray = teams.length / 2;
 var fixtures = [];
 
 function fixtureAlgorithm() {
   
-  if(teams.length % 2 == 1) {
+  // For an odd number of teams introduce a bye fixture into each fixture round
+  if (teams.length % 2 == 1) {
     teams.push("bye fixture");
   }
   
-  let middleOfTeamsArray = teams.length / 2;
-  console.log(middleOfTeamsArray);
-
-  for(let i = 0; i < middleOfTeamsArray; i++){
-    fixtures.push(teams[i] + " vs " + teams[teams.length - 1 - i]);
+  // Create one fewer rounds than the number of teams
+  for (let i = 0; i < teams.length - 1; i++) {
+    
+    // Pair the first team with last team, second with penultimate, etc...
+    for (let j = 0; j < middleOfTeamsArray; j++) {
+      fixtures.push(teams[j] + " vs " + teams[teams.length - 1 - j]);
+    }
+    
+    // Mark the next round of fixtures but not end of last round
+    if(i != teams.length - 2){
+      fixtures.push("next round of fixtures");
+    }
+    
+    // Move last array team to second array position to create new fixtures  
+    let lastTeam = teams.pop();
+    teams.splice(1, 0, lastTeam);
   }
-  console.log(fixtures);
+  
+  // Loop to print all fixtures
+  for (let k = 0; k < fixtures.length; k++) {
+    console.log(fixtures[k]);
+  }
 }
-
-fixtureAlgorithm();
-
-
